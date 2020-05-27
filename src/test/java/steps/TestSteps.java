6e7c5248e508605b40bf4a6ec7c3c7b800ebb465
Prashant Ramcharan <info@prashant-ramcharan.co.uk>
2020-05-27 10:50:35 +0100

    Changed to lambda step defs

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -3,9 +3,7 @@ package steps;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 import io.github.pramcharan.wd.binary.downloader.WebDriverBinaryDownloader;
 import io.github.pramcharan.wd.binary.downloader.enums.BrowserType;
 import org.openqa.selenium.OutputType;
@@ -13,7 +11,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestSteps {
+public class TestSteps implements En {
     private WebDriver driver;
 
     @Before
@@ -35,26 +33,24 @@ public class TestSteps {
         }
     }
 
-    @Given("^I navigate to Stack Overflow$")
-    public void iNavigateToStackOverflow() {
-        driver.navigate().to("https://stackoverflow.com/");
-    }
+    public TestSteps() {
+        When("^I navigate to Stack Overflow$", () -> {
+            driver.navigate().to("https://stackoverflow.com/");
+        });
 
-    @When("I navigate to Stack Overflow question page {int}")
-    public void i_navigate_to_Stack_Overflow_question_page(Integer page) {
-        driver.navigate().to("https://stackoverflow.com/questions?page=" + page);
-    }
+        When("^I navigate to Stack Overflow question page (\\d+)$", (Integer page) -> {
+            driver.navigate().to("https://stackoverflow.com/questions?page=" + page);
+        });
 
-    @Then("I verify Stack Overflow question page {int} is opened")
-    public void i_verify_Stack_Overflow_question_page_is_opened(Integer page) {
-        if (!driver.getTitle().contains("Page " + page)) {
-            throw new RuntimeException("The Stack Overflow page title does not contain the page number: " + page);
-        }
-    }
+        Then("^I verify Stack Overflow question page (\\d+) is opened$", (Integer page) -> {
+            if (!driver.getTitle().contains("Page " + page)) {
+                throw new RuntimeException("The Stack Overflow page title does not contain the page number: " + page);
+            }
+        });
 
-    @When("I use the following data table to navigate to a Stack Overflow question page")
-    public void i_use_the_following_data_table_to_navigate_to_a_Stack_Overflow_question_page(io.cucumber.datatable.DataTable dataTable) {
-        int pageNumber = Integer.valueOf(dataTable.cell(1, 0));
-        i_navigate_to_Stack_Overflow_question_page(pageNumber);
+        When("^I use the following data table to navigate to a Stack Overflow question page$", (io.cucumber.datatable.DataTable dataTable) -> {
+            int pageNumber = Integer.parseInt(dataTable.cells().get(1).get(0));
+            driver.navigate().to("https://stackoverflow.com/questions?page=" + pageNumber);
+        });
     }
 }
